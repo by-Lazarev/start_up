d637c34ef574da88c6ac0c96cc4f1975b86e3cc4
Antoinette Barrett <antoinet@oa-h4.msk.21-school.ru>
2023-04-25 16:25:14 +0300

    start updated

--- start.sh
+++ start.sh
@@ -3,5 +3,3 @@ mkdir -p ~/goinfre/Docker/Data
 ln -s ~/goinfre/Docker ~/Library/Containers/com.docker.docker
 git clone https://github.com/Homebrew/brew goinfre/homebrew
 omz reload
-brsw
-brew update
