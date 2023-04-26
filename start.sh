37a1d7af874d9375fa3007b69c8fa30d25f60ac8
Antoinette Barrett <antoinet@su-c4.msk.21-school.ru>
2023-04-26 11:25:04 +0300

    lil fix

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-m -rf ~/Library/Containers/com.docker.docker
+rm -rf ~/Library/Containers/com.docker.docker
 mkdir -p ~/goinfre/Docker/Data
 ln -s ~/goinfre/Docker ~/Library/Containers/com.docker.docker
-git clone https://github.com/Homebrew/brew goinfre/homebrew
+git clone https://github.com/Homebrew/brew ~/goinfre/homebrew
 omz reload
